8f70b6baafc1c0ddd6350c8bacd7f09db7f6c124
Michael Mikulak <mpmikulak@gmail.com>
2024-10-10 18:19:41 -0500

    update fibs

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -1,10 +1,25 @@
+# Numbers are cool, right?
+# I think so, so let's make some
 def fibonacci(num):
-    if num < 1:
+    int_num = int(num)
+    if int_num < 1:
         return 1
-    nums = [1, 2]
-    for i in range(1, num):
-        nums.append(i + nums[i - 1])
+    elif int_num == 1 or int_num == 2:
+        return 1
+
+    nums = [0, 1]
+    for i in range(2, int_num + 1):
+        nth = nums[i - 1] + nums[i - 2]
+        nums.append(nth)
     return nums.pop()
 
 
-print(fibonacci(3))
+def parse(target_num):
+    if target_num.isnumeric():
+        return target_num
+    else:
+        print("Enter a number, please.")
+
+
+target_num = input("Which Fibonacci number do you want?")
+print(fibonacci(parse(target_num)))
